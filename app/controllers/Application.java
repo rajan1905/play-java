05238e7a04d39b07363d2bcf5230e0222c51f32f
rajan1905 <rajan_1905@live.com>
2014-07-05 17:00:18 +0530

    Update_6July_1

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("aaaaaaaaaaaa"));
+        return ok(index.render("KHAWJA HOMES REAL ESTATE"));
     }
 
 }
