12225a8437ce8b78d0720a1c58a5a9d214105b9f
rajan1905 <rajan_1905@live.com>
2014-07-05 19:11:50 +0530

    Updat_6July_6
    Contact Page Working

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,6 +9,12 @@ public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("KHAWJA HOMES REAL ESTATE"));
+       
     }
-
+     public static Result about() {
+        return ok(about.render("KHAWJA HOMES REAL ESTATE"));
+     }
+      public static Result contact() {
+        return ok(contact.render("KHAWJA HOMES REAL ESTATE"));
+     }
 }
