410ce1be5cc19c8c01de3f58506f29ffaaa7dba8
rajan1905 <rajan_1905@live.com>
2014-07-18 17:56:33 +0530

    18July_3

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,14 +8,14 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("KHAWJA HOMES REAL ESTATE"));
+        return ok(index.render("KHAWAJA HOMES REAL ESTATE"));
        
     }
      public static Result about() {
-        return ok(about.render("KHAWJA HOMES REAL ESTATE"));
+        return ok(about.render("KHAWAJA HOMES REAL ESTATE"));
      }
       public static Result contact() {
-        return ok(contact.render("KHAWJA HOMES REAL ESTATE"));
+        return ok(contact.render("KHAWAJA HOMES REAL ESTATE"));
      }
      
 }
